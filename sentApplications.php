1fc8ec36593dbfaa8b831bd9668ec578d610536c
sergeg24 <sergeg24@mail.ru>
2016-05-05 14:11:14 +0300

    1) Добавлена версия программы 2) Добавлена возможность скачивать файл 3) Поправлена логика вывода файла

--- sentApplications.php
+++ sentApplications.php
@@ -12,21 +12,52 @@ $members = array(
 	'Administrator',
 );
 
+$version = "1.0.1";
+
+$panel = "<div id='panel'><a href='?download'>download</a></a>";
+
+function file_get_contents_utf8($fn) { 
+     $content = file_get_contents($fn); 
+      return mb_convert_encoding($content, 'UTF-8', 
+          mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true)); 
+} 
+
 $eventName = $modx->event->name;
   switch($eventName) {
 	  case 'OnPageNotFound':
 	    if ($modx->context->key == 'mgr') return;
-		$file_name = str_replace(array('/'), '', $_SERVER['REQUEST_URI']);
+		$file_name = preg_replace(array("/\?(.*)/", "/\//"), '', $_SERVER['REQUEST_URI']);
 		$SERVER_NAME = 	$name_virtual_file ? $name_virtual_file : $_SERVER['SERVER_NAME'];
 		if($modx->user->isMember($members) && $file_name == $SERVER_NAME){
 			$dir = $_SERVER['DOCUMENT_ROOT'].$dir.$_SERVER['SERVER_NAME'].'.send';
+			if(isset($_GET['download'])){
+				file_force_download($dir);
+			}
 			if(file_exists($dir)){
-				$file = file_get_contents($dir);
 				$file = iconv('windows-1251', 'utf-8', $file);
 				$file = htmlspecialchars($file);
+				echo "<em>version ".$version."</em>";
+				echo $panel;
 				echo '<pre>'.$file.'</pre>';
 				exit();
 			}
 		}
 	  break;
   }
+
+function file_force_download($file) {
+  if (file_exists($file)) { 
+    if (ob_get_level()) { 
+      ob_end_clean(); 
+    } 
+    header('Content-Description: File Transfer'); 
+    header('Content-Type: application/octet-stream'); 
+    header('Content-Disposition: attachment; filename=' . basename($file.'.txt')); 
+    header('Content-Transfer-Encoding: binary'); 
+    header('Expires: 0'); 
+    header('Cache-Control: must-revalidate'); 
+    header('Pragma: public'); 
+    header('Content-Length: ' . filesize($file)); 
+    readfile($file); 
+  } 
+}
