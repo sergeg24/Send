73b033f238fe05a45d4271f8a83f2474b1066251
sergeg24 <sergeg24@mail.ru>
2016-07-06 08:57:39 +0300

    Новая версия send. Подробнее: http://ima-pr.ru/js/archives/send/

---
+++ send.1.6.4.js
@@ -0,0 +1,198 @@
+//version 1.6.4
+//manual: http://ima-pr.ru/js/archives/send/
+//onclick="formSend(id);return false;";
+function formSend(id){
+	var inputVal,fieldName='',countCheck,type,typeInp='',button,max_upload_file,c,name,checked_obj,default_message,objectElement,url,input,label,fNameA,emptyElement,formTegId,test,
+	idInp,one,fieldNameA,nameArray,requestRequired,checked,val,classErr,classValid,patternID = /_[^|]*$/i,ElementShift,idResult,fd,file_data,
+	classResult,send_request_always,elementId,countElement,request,hidden_form_teg = 'hide_'+id,hidden_form = false,patternEmail = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+	/*~~~OPTION~~~*/
+	url = "/js/archives/send/send-test.php";//путь до файла отправки почты	
+	max_upload_file = 2;//максимально допустимый размер загружаемого файла (Мб)
+	formTegId = "#form_"+id;//id на тег <form>	
+	idResult = "#result_"+id;//вывод результата в блок	
+	classResult = "result";//доп. класс. Вставляется на блок "idResult"	
+	send_request_always = true;//(true) всегда отправляет запрос к обработчику, даже если заполнены не все обязательные поля		
+	one = "1";//то что толжен вернуть обработчик в случае успешной проверки формы (echo "1";)	
+	classErr = "f_err";//класс пустого поля		
+	classValid = "f_valid";//класс заполненного поля		
+	default_message = {
+		"no_id":"Укажите id формы!",
+		"good":"<div class='good'><span>Сообщение успешно отправлено!</span></div>"
+	};
+	/*~~~OPTION~~~*/
+	if($.type(id) === 'undefined' || id == ''){
+		console.log(default_message.no_id);
+		return false;
+	}
+	fd = new FormData($(formTegId)[0]);
+	fd.append("form", id);
+	countElement = 0;
+	test = [];
+	requestRequired = [];
+	request = [];
+	button = $(formTegId + ' input[type="submit"],'+formTegId+' button');
+	//Собираем все поля в два массива, все поля и обязательные поля
+	$(formTegId + " *").each(function(){
+		objectElement = $(this);
+		elementId = objectElement.attr("id");
+		if($.type(elementId) !== 'undefined'){
+			if($(this).hasClass( "required" )){
+				requestRequired.push("#"+elementId);
+			}
+			type = $(this)[0].type;
+			if($.type(type) !== 'undefined'){
+				typeInp = "|" + $(this)[0].type;
+			}else{
+				typeInp = '';
+			}
+			request.push(elementId + typeInp);
+		}				
+	});
+	if($('body').find('.' + hidden_form_teg)){
+		hidden_form = true;
+		$('.' + hidden_form_teg).show();
+	}
+	//подгатавливаем ВСЕ запросы
+	for (var i = 0; i < request.length; i++){
+		fieldNameA = request[i];
+		nameArray = fieldNameA.split("|");
+		name = nameArray[0].replace(patternID,'');
+		type = nameArray[1];
+		countCheck = nameArray.length;
+		if(type == 'checkbox'){
+			checked_obj = $("#"+nameArray[0]+":checkbox:checked");	
+			c = 1;
+		}else if(type == 'radio'){
+			checked_obj = $("#"+nameArray[0]+":radio:checked");
+			c = 1;
+		}else{
+			val = $("#"+nameArray[0]).val();
+			c = 0;
+		}	
+		if(c){
+			checked = checked_obj.length;
+			if(checked == 1){
+				$("#"+nameArray[0]).val(1);
+			}else{
+				$("#"+nameArray[0]).val('');
+			}
+			val = checked;
+		}	
+		if(type == 'file'){
+			file_data = $("#"+nameArray[0]).prop('files')[0];
+			if($.type(file_data) !== 'undefined'){
+				if(max_upload_file * 1000000 < file_data.size){
+					$("#"+nameArray[0]).val('');
+				}
+				fd.append(name, file_data);
+			}	
+		}
+		if(countCheck == 1) continue;
+		fd.append(name, val);
+	}
+	//производим проверку обязательных полей
+	for (var i = 0; i < requestRequired.length; i++){
+		countElement++;
+		idInp = requestRequired[i];
+		input = $(idInp);
+		inputVal = input.val();	
+		fNameA = idInp.replace(patternID,'');				
+	if(inputVal==""){
+		test.unshift(idInp);
+		input.addClass(classErr).removeClass(classValid);						
+		$(idInp + " + label").remove();
+	}else if(fNameA == "#email" || fNameA == "#mail"){
+		if(!patternEmail.test($(idInp).val())){
+			test.unshift(idInp);
+			input.addClass(classErr).removeClass(classValid);						
+			$(idInp + " + label").remove();
+		}else{
+			input.addClass(classValid).removeClass(classErr);						
+			$(idInp + " + label").remove();
+		}	
+	}else{
+			input.addClass(classValid).removeClass(classErr);
+			$(idInp + " + label").remove();
+		}
+	} 
+	emptyElement = test.reverse();
+	ElementShift = emptyElement.shift();//id		
+	if($.type(ElementShift) != 'undefined'){
+		if($.type($(ElementShift).attr('title')) != 'undefined'){
+			label = "<label class='messageLabel'>Заполните поле: <span>" + $(ElementShift).attr('title') + "!</span></label>";
+		}
+		$(ElementShift).focus().after(label);
+		if(send_request_always == false){
+			return false;
+		}
+	}
+	var result_text = '';
+	var send = false;
+	
+	var action = $(formTegId).attr('action');
+	
+	if(action != '' && $.type(action) !== 'undefined'){
+		url = action;
+	}
+	
+	$.ajax({	
+		type: "POST",
+		url: url,
+		data: fd,
+		contentType: false,
+		processData: false,
+		dataFilter: function(jsonString){
+			if(/^\{.*\}$/.exec(jsonString)){
+				return jsonString;
+			}else{
+				var event = {};
+				if(jsonString == one){
+					event.good = default_message.good;
+				}else{	
+					event.error = jsonString;
+				}
+				return JSON.stringify(event);
+			}
+		},
+		dataType: "json",	
+		success: function(data){
+			$(idResult).empty();	
+			if($.type(data.good) !== 'undefined'){
+				result_text = data.good;
+				send = true;
+			}else if($.type(data.error) !== 'undefined'){
+				result_text = data.error;
+			}
+			if(send){
+				button.prop('disabled', true);
+				$(formTegId).trigger('reset');
+				input.removeClass(classValid);
+				$(idResult).append(result_text);
+				$(idResult+" .good")
+					.fadeIn()
+					.delay(2000)
+					.fadeOut();
+				$(idResult)
+					.fadeIn()
+					.delay(2000)
+					.fadeOut('show', function(){
+						button.prop('disabled', false);
+					});
+				if(hidden_form){
+					$('.' + hidden_form_teg)
+						.delay(2000)
+						.fadeOut();
+				}
+			}else{
+				button.prop('disabled', true);
+				$(idResult)
+					.fadeIn()
+					.append(result_text)
+					.delay(2000)
+					.fadeOut('show', function(){
+						button.prop('disabled', false);
+					});	
+			}
+		}
+	});
+}
