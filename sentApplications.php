b01645dd16ca080c4043c78968c0ee4837cc8c51
Сергей <sergeg24@mail.ru>
2017-03-11 20:18:00 +0300

    Изменена кодировка файла на utf8

--- sentApplications.php
+++ sentApplications.php
@@ -1,15 +1,15 @@
 <?php
-//Îòîáðàæàåò îòïðàâëåííûå ñ ôîðì çàÿâêè
+//ÐžÑ‚Ð¾Ð±Ñ€Ð°Ð¶Ð°ÐµÑ‚ Ð¾Ñ‚Ð¿Ñ€Ð°Ð²Ð»ÐµÐ½Ð½Ñ‹Ðµ Ñ Ñ„Ð¾Ñ€Ð¼ Ð·Ð°ÑÐ²ÐºÐ¸
 
-//Ïóòü äî ïàïêè â êîòîðîé ëåæèò ôàéë ñ ñîõðàíåíûìè çàÿâêàìè
+//ÐŸÑƒÑ‚ÑŒ Ð´Ð¾ Ð¿Ð°Ð¿ÐºÐ¸ Ð² ÐºÐ¾Ñ‚Ð¾Ñ€Ð¾Ð¹ Ð»ÐµÐ¶Ð¸Ñ‚ Ñ„Ð°Ð¹Ð» Ñ ÑÐ¾Ñ…Ñ€Ð°Ð½ÐµÐ½Ñ‹Ð¼Ð¸ Ð·Ð°ÑÐ²ÐºÐ°Ð¼Ð¸
 $dir = '/js/';
 
-//Èìÿ âèðòóàëüíîãî ôàéëà, ïî óìîë÷àíèþ èìÿ_äîìåíà
+//Ð˜Ð¼Ñ Ð²Ð¸Ñ€Ñ‚ÑƒÐ°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ñ„Ð°Ð¹Ð»Ð°, Ð¿Ð¾ ÑƒÐ¼Ð¾Ð»Ñ‡Ð°Ð½Ð¸ÑŽ Ð¸Ð¼Ñ_Ð´Ð¾Ð¼ÐµÐ½Ð°
 $name_virtual_file = 'send.txt';
 
-//Êàêèå ãðóïïû ìîãóò âèäåòü ôàéë
+//ÐšÐ°ÐºÐ¸Ðµ Ð³Ñ€ÑƒÐ¿Ð¿Ñ‹ Ð¼Ð¾Ð³ÑƒÑ‚ Ð²Ð¸Ð´ÐµÑ‚ÑŒ Ñ„Ð°Ð¹Ð»
 $members = array(
-	'Administrator',
+    'Administrator',
 );
 
 $version = "1.0.1";
@@ -17,40 +17,40 @@ $version = "1.0.1";
 $panel = "<div id='panel'><a href='?download'>download</a></a>";
 
 $eventName = $modx->event->name;
-  switch($eventName) {
-	  case 'OnPageNotFound':
-	    if ($modx->context->key == 'mgr') return;
-		$file_name = preg_replace(array("/\?(.*)/", "/\//"), '', $_SERVER['REQUEST_URI']);
-		$SERVER_NAME = 	$name_virtual_file ? $name_virtual_file : $_SERVER['SERVER_NAME'];
-		if($modx->user->isMember($members) && $file_name == $SERVER_NAME){
-			$dir = $_SERVER['DOCUMENT_ROOT'].$dir.$_SERVER['SERVER_NAME'].'.send';
-			if(file_exists($dir)){
-				$file = file_get_contents($dir); 	
-				$file = iconv('windows-1251', 'utf-8', $file);
-				if(isset($_GET['download'])){
-					file_force_download($file);
-				}
-				echo "<em>version ".$version."</em>";
-				echo $panel;
-				echo '<pre>'.$file.'</pre>';
-				exit();
-			}
-		}
-	  break;
-  }
+switch($eventName) {
+    case 'OnPageNotFound':
+        if ($modx->context->key == 'mgr') return;
+        $file_name = preg_replace(array("/\?(.*)/", "/\//"), '', $_SERVER['REQUEST_URI']);
+        $SERVER_NAME = 	$name_virtual_file ? $name_virtual_file : $_SERVER['SERVER_NAME'];
+        if($modx->user->isMember($members) && $file_name == $SERVER_NAME){
+            $dir = $_SERVER['DOCUMENT_ROOT'].$dir.$_SERVER['SERVER_NAME'].'.send';
+            if(file_exists($dir)){
+                $file = file_get_contents($dir);
+                $file = iconv('windows-1251', 'utf-8', $file);
+                if(isset($_GET['download'])){
+                    file_force_download($file);
+                }
+                echo "<em>version ".$version."</em>";
+                echo $panel;
+                echo '<pre>'.$file.'</pre>';
+                exit();
+            }
+        }
+        break;
+}
 
 function file_force_download($content) {
-	global $name_virtual_file;
-	header('Content-Description: File Transfer'); 
-    header('Content-Type: application/octet-stream'); 
-    header('Content-Disposition: attachment; filename=' . basename($name_virtual_file)); 
-   	header('Content-Transfer-Encoding: binary'); 
-    header('Expires: 0'); 
-    header('Cache-Control: must-revalidate'); 
-    header('Pragma: public'); 
-	ob_end_clean();
-	ob_start();
-	echo str_replace("[", "\r\n[", $content);
-	ob_end_flush();
-	exit();
+    global $name_virtual_file;
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename=' . basename($name_virtual_file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    ob_end_clean();
+    ob_start();
+    echo str_replace("[", "\r\n[", $content);
+    ob_end_flush();
+    exit();
 }
